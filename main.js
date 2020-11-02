e70e06d68bf3326ed068044211f5f8349391cd40
Alessandro Ferraro <ale_ferraro@libero.it>
2020-11-02 15:57:12 +0100

    Show/Hide Solution
    Implemented solution that shows and hides the menu on clicking the hamburger-menu icon using jquery

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+Hamburger menu:
+mostrare / nascondere il menu principale
+(ricordate che per vedere l'hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+Partite come base del vostro esercizio scaricando questa repo:
+https://bitbucket.org/booleancareers/ex-hamburger-menu-base/downloads
+Ricordate di scrivere solo il codice JavaScript con jQuery e non modificare HTML e CSS
+*/
+
+$('.header-right > a').click(function(){
+  $('.hamburger-menu').show();
+})
+
+$('.hamburger-menu > .close').click(function(){
+  $('.hamburger-menu').hide();
+})
